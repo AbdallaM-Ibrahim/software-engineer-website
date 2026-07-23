b46f55593c917587dea27fcf35869f4df9b4fead
Abdalla Mostafa <Abdalla.mostafa19200@gmail.com>
2026-07-23 15:34:02 +0300

    fix: update textarea component for consistent styling and attributes

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -5,9 +5,9 @@ import { cn } from "@/lib/utils";
 function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
   return (
     <textarea
-      data-slot="textarea"
+      data-slot='textarea'
       className={cn(
-        "border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 aria-invalid:border-destructive flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        'border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 aria-invalid:border-destructive flex field-sizing-content min-h-16 max-h-33.5 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm overflow-auto resize-none',
         className,
       )}
       {...props}
